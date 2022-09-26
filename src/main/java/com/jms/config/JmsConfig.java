4c305e38dd20a3b6ec44d3c5196c1fc8f97561d8
adrianpusty <adrianpusty>
2022-09-26 12:21:49 +0200

    solution source mentioned for MessageConversionException

--- src/main/java/com/jms/config/JmsConfig.java
+++ src/main/java/com/jms/config/JmsConfig.java
@@ -19,6 +19,7 @@ public class JmsConfig
 
         ObjectMapper mapper = new ObjectMapper();
         // needed to resolve: org.springframework.jms.support.converter.MessageConversionException: Could not map JSON object [CommissionedPrice(id=106, instrumentName=EUR/USD, bid=1.1988000, ask=1.1011000, timestamp=2020-06-01T12:01:01.001)]; nested exception is com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Java 8 date/time type `java.time.LocalDateTime` not supported by default: add Module "com.fasterxml.jackson.datatype:jackson-datatype-jsr310" to enable handling (through reference chain: com.domain.CommissionedPrice["timestamp"])
+        // solution from here: https://github.com/FasterXML/jackson-modules-java8#registering-modules
         mapper.findAndRegisterModules();
         converter.setObjectMapper(mapper);
 
